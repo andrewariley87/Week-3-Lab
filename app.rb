3df921edd7b7135ad5c19be7c78a3b860f4cc0e9
andrewariley87 <andrewariley87@gmail.com>
2015-07-20 16:04:15 -0400

    Dynamic
    First stab at erb templates

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/current_time' do
+  "<h1>It is currently #{Time.now}.</h1>"
+end
+
+get '/template' do
+ipsum = ["Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.",
+  "Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.",
+  "Out believe has request not how comfort evident. Up delight cousins we feeling minutes. Genius has looked end piqued spring. Down has rose feel find man. Learning day desirous informed expenses material returned six the. She enabled invited exposed him another. Reasonably conviction solicitude me mr at discretion reasonable. Age out full gate bed day lose."
+]
+  erb :index, :locals => {:ipsum => ipsum}
+end
